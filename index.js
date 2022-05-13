1d3a76d3c221764f674b9537a6b01d59894fcb5f
nityadyuthi <nityaa55@gmail.com>
2022-05-13 15:09:36 +0530

    Add initial files

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+var loggedIn = false;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  if (loggedIn) {
+    res.redirect("/");
+  }
+  res.render("login");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
