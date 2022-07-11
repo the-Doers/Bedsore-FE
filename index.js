7991fd61b28d2203a2293d396fded216ed92aee2
Likith <likithsrinath2000@gmail.com>
2022-07-11 12:06:22 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ else {
   var db_config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DB_DATABASENAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_USERNAME,
   };
 }
 
